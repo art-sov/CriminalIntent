98ebb28ebe89e30c4ea7cbd899710fdd809b7e05
art-sov <artsovlov@gmail.com>
2018-07-04 14:16:27 +0300

    chapter13 complit

--- app/src/main/java/com/art/criminalintent/CrimeLab.java
+++ app/src/main/java/com/art/criminalintent/CrimeLab.java
@@ -22,13 +22,7 @@ public class CrimeLab {
 
     private CrimeLab (Context context){
         mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
         }
-    }
 
     public List<Crime> getCrimes(){
         return mCrimes;
@@ -41,6 +35,10 @@ public class CrimeLab {
             }
         }
         return null;
+    }
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 
 
